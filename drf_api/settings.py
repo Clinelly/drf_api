adef6ee40f9e6db7b7fcb26d7126015c8edda8c9
Clinelly <swall289@gmail.com>
2023-04-27 10:33:58 +0000

    Add profile model

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00t(2z_t0ea1gv^am3ghd-em8=sf&2g&zhyr37q8kz4&ue^w0@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'profiles',
 ]
 
 MIDDLEWARE = [
